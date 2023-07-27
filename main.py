862fef50953764bb10fd53f7ddd30b68cb7496c8
Michał Mizera <axadiw@gmail.com>
2023-07-27 18:55:03 +0200

    localstorage test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import urllib
 
 import streamlit as st
+import uuid
+from streamlit_ws_localstorage import injectWebsocketCode
 
 from cookies import COOKIES_PREFIX
 from lib.streamlit_cookies_manager import CookieManager
@@ -8,6 +10,15 @@ from the_pages.Account import account
 from the_pages.Market import market
 
 st.set_page_config(page_title='Project Miami', page_icon='💵', layout='wide')
+
+
+HOST_PORT = 'miamiws.axadiw.pl'
+conn = injectWebsocketCode(hostPort=HOST_PORT, uid=str(uuid.uuid1()))
+ret = conn.setLocalStorageVal(key='k1', val='v1')
+# ret = conn.getLocalStorageVal(key='k1')
+
+
+
 session = st.runtime.get_instance()._session_mgr.list_active_sessions()[0]
 st_base_url = urllib.parse.urlunparse([session.client.request.protocol, session.client.request.host, "", "", "", ""])
 cookies = CookieManager(
