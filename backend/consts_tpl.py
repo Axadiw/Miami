b124adf52062d873a5498edc305c66626cea07e4
Michał Mizera <axadiw@gmail.com>
2023-08-25 21:46:52 +0200

    Fixed docekr secrets read

--- backend/consts_tpl.py
+++ backend/consts_tpl.py
@@ -1,6 +1,10 @@
 from get_docker_secret import get_docker_secret
 
-db_username = get_docker_secret('MIAMI_POSTGRES_USER') or 'op://Private/Miami Local Postgres Root Password/username'
-db_password = get_docker_secret('MIAMI_POSTGRES_PASSWORD') or 'op://Private/Miami Local Postgres Root Password/password'
-db_name = get_docker_secret('MIAMI_POSTGRES_DB') or 'op://Private/Miami Local Postgres Root Password/database'
-flask_api_secret = get_docker_secret('MIAMI_FLASK_SECRET') or 'op://Private/Miami Flask Secret Key Local/password'
+db_username = get_docker_secret('MIAMI_POSTGRES_USER',
+                                autocast_name=False) or 'op://Private/Miami Local Postgres Root Password/username'
+db_password = get_docker_secret('MIAMI_POSTGRES_PASSWORD',
+                                autocast_name=False) or 'op://Private/Miami Local Postgres Root Password/password'
+db_name = get_docker_secret('MIAMI_POSTGRES_DB',
+                            autocast_name=False) or 'op://Private/Miami Local Postgres Root Password/database'
+flask_api_secret = get_docker_secret('MIAMI_FLASK_SECRET',
+                                     autocast_name=False) or 'op://Private/Miami Flask Secret Key Local/password'
