ef72821b1659b8e4aae4ea2fe26e9d74c00608ab
Michał Mizera <axadiw@gmail.com>
2023-08-25 23:43:45 +0200

    Added versioning

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from consts_secrets import db_username, db_password, db_name
 from consts_tpl import flask_api_secret
 from database import db
 from endpoints.account import account_routes
+from endpoints.general import general_routes
 from endpoints.session import session_routes
 
 if not db_username or not db_password or not db_name:
@@ -14,6 +15,7 @@ if not db_username or not db_password or not db_name:
 def register_extensions(app):
     app.register_blueprint(account_routes)
     app.register_blueprint(session_routes)
+    app.register_blueprint(general_routes)
 
 
 def prepare_app():
--- backend/consts_tpl.py
+++ backend/consts_tpl.py
@@ -8,3 +8,4 @@ db_name = get_docker_secret('MIAMI_POSTGRES_DB',
                             autocast_name=False) or 'op://Private/Miami Local Postgres Root Password/database'
 flask_api_secret = get_docker_secret('MIAMI_FLASK_SECRET',
                                      autocast_name=False) or 'op://Private/Miami Flask Secret Key Local/password'
+miami_version_env_key = "MIAMI_VERSION"
