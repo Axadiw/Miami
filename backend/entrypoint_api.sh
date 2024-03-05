d7c5405929870fe45ed7c666971f3419e04a9b73
Michał Mizera <axadiw@gmail.com>
2024-03-05 15:53:49 +0100

    Added port for socketio server

--- backend/entrypoint_api.sh
+++ backend/entrypoint_api.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 poetry run alembic -c shared/alembic.ini upgrade heads
-sleep 3600
-#poetry run waitress-serve --host 0.0.0.0 --port 5000 --call api.app:create_app
+poetry run waitress-serve --host 0.0.0.0 --port 5000 --call api.app:create_app
