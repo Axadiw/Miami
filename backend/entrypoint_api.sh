7fe3e356c4938add404ac30f6ab2cf0a17471818
Michał Mizera <axadiw@gmail.com>
2024-03-05 19:24:11 +0100

    api sleep

--- backend/entrypoint_api.sh
+++ backend/entrypoint_api.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 poetry run alembic -c shared/alembic.ini upgrade heads
-poetry run waitress-serve --host 0.0.0.0 --port 5000 --call api.app:create_app
+sleep 90000
+#poetry run waitress-serve --host 0.0.0.0 --port 5000 --call api.app:create_app
