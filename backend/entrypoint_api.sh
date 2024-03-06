90f34fa2ef6176f08c7da310fa39a27f16ea9ec5
Michał Mizera <axadiw@gmail.com>
2024-03-06 16:20:43 +0100

    gceate

--- backend/entrypoint_api.sh
+++ backend/entrypoint_api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 poetry run alembic -c shared/alembic.ini upgrade heads
-poetry run gunicorn -b :5000 -k eventlet api.app:app
+poetry run gunicorn -b :5000 -k eventlet api.app:gunicorn_create
